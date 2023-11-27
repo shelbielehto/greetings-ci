6e2e75beded126c69d8f89c58439696cb7564fa5
shelbielehto <142325845+shelbielehto@users.noreply.github.com>
2023-11-27 15:01:05 -0500

    fix: test-script.sh typo

--- test-script.sh
+++ test-script.sh
@@ -4,5 +4,5 @@ set -e
 java -jar build/libs/greetings-ci-$1.jar ${@:2} > output.bench
 IFS=' ' read -ra ARR <<< "${@:2}"
 for i in "${ARR[@]}"; do
-grep "^$i$" output.bench
+  grep "^$i$" output.bench
 done
